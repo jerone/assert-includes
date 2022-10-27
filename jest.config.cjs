85d63e7e9706a1b2aecd302f6aca9101581b06d9
Jeroen van Warmerdam <jeronevw@hotmail.com>
2022-10-27 18:38:30 +0200

    Add coverage and CI

--- jest.config.cjs
+++ jest.config.cjs
@@ -2,12 +2,31 @@
  * For a detailed explanation regarding each configuration property, visit:
  * https://jestjs.io/docs/en/configuration.html
  */
-
+/** @type {import('jest').Config} */
 module.exports = {
+  collectCoverage: true,
+  collectCoverageFrom: ["index.{js,cjs}"],
   coverageDirectory: "coverage",
   coverageProvider: "v8",
+  coverageReporters: ["text", "text-summary", "lcov", "cobertura"],
   testEnvironment: "node",
+  reporters: [
+    "default",
+    [
+      // Needed for CI workflow Github Action Job Summary.
+      // See https://github.com/dorny/test-reporter#supported-formats
+      "jest-junit",
+      {
+        outputDirectory: "coverage",
+        ancestorSeparator: " › ",
+        uniqueOutputName: "false",
+        suiteNameTemplate: "{filepath}",
+        classNameTemplate: "{classname}",
+        titleTemplate: "{title}",
+      },
+    ],
+  ],
   transform: {
-    ".js": './jest-esm-transformer.cjs',
+    ".js": "./jest-esm-transformer.cjs",
   },
 };
